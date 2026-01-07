b1423f9c4f92212c58e5de02fa214ff1e1498b0b
ManojSravan <manojsravan2002@gmail.com>
2026-01-07 13:08:46 +0530

    minor changes

--- .next/dev/static/chunks/app_page_tsx_58591f76._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_5241a45a._.js",
-  "static/chunks/_5152d543._.js"
-],
-    source: "dynamic"
-});
